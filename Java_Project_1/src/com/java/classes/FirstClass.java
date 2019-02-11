88a902d844bdf386197202cafb61badfcf9be11d
kavvya <mkavya06@gmail.com>
2019-02-11 02:05:24 -0500

    Basic java classes

---
+++ Java_Project_1/src/com/java/classes/FirstClass.java
@@ -0,0 +1,10 @@
+package com.java.classes;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi!");
+	}
+
+}
---
+++ Java_Project_1/src/com/java/classes/ThirdClass.java
@@ -0,0 +1,12 @@
+package com.java.classes;
+
+public class ThirdClass 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("How are you?");
+		 
+	}
+
+}
