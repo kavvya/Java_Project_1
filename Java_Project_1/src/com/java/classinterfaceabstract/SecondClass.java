93fb30c2761ece23def4a7007960c928f1c1cc0d
kavvya <mkavya06@gmail.com>
2019-02-11 02:01:00 -0500

    Added basic java project
    Added basic java project

---
+++ Java_Project_1/src/com/java/classinterfaceabstract/SecondClass.java
@@ -0,0 +1,10 @@
+package com.java.classinterfaceabstract;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello !");
+	}
+
+}
---
+++ Java_Project_1/src/com/java/classinterfaceabstract/ThirdClass.java
@@ -0,0 +1,12 @@
+package com.java.classinterfaceabstract;
+
+public class ThirdClass 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("HRU !");
+		 
+	}
+
+}
